707b9d8e8d9ab7d141d57ef8c4fb29c2bd88072a
niyioo <niyiolagbegi@gmail.com>
2023-07-24 12:05:28 -0700

    fix

--- sort.h
+++ sort.h
@@ -2,6 +2,8 @@
 #define SORT_H
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -21,5 +23,7 @@ void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */
