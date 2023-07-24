53e03ca25b42f3f849c95279c8a7745cd58b70b3
niyioo <niyiolagbegi@gmail.com>
2023-07-24 12:37:48 -0700

    fix

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,10 +1,10 @@
 #include "sort.h"
 
 /**
- * swap - Swaps two integers
+ * swap - Swaps two elements in an array
  *
- * @a: Pointer to the first integer
- * @b: Pointer to the second integer
+ * @a: Pointer to the first element to be swapped
+ * @b: Pointer to the second element to be swapped
  */
 void swap(int *a, int *b)
 {
@@ -14,21 +14,20 @@ void swap(int *a, int *b)
 }
 
 /**
- * lomuto_partition - Lomuto partition scheme for Quick sort
+ * partition - Lomuto partition scheme for Quick sort
  *
- * @array: The array to be sorted
- * @low: The starting index of the partition
- * @high: The ending index of the partition
+ * @array: The array to be partitioned
+ * @low: Starting index of the partition
+ * @high: Ending index of the partition
  *
- * Return: The final pivot index
+ * Return: Index of the pivot element
  */
-int lomuto_partition(int *array, int low, int high)
+int partition(int *array, int low, int high)
 {
 	int pivot = array[high];
-	int i = low - 1;
-	int j;
+	int j, i = low - 1;
 
-	for (j = low; j <= high - 1; j++)
+	for (j = low; j < high; j++)
 	{
 		if (array[j] <= pivot)
 		{
@@ -40,32 +39,31 @@ int lomuto_partition(int *array, int low, int high)
 			}
 		}
 	}
-
-	if ((i + 1) != high)
+	if (i + 1 != high)
 	{
 		swap(&array[i + 1], &array[high]);
 		print_array(array, high + 1);
 	}
-
 	return (i + 1);
 }
 
 /**
- * quick_sort_recursive - Recursive function for Quick sort
+ * quick_sort_recursive - Recursive Quick sort function
  *
  * @array: The array to be sorted
- * @low: The starting index of the partition
- * @high: The ending index of the partition
+ * @low: Starting index of the array to be sorted
+ * @high: Ending index of the array to be sorted
  */
 void quick_sort_recursive(int *array, int low, int high)
 {
-	int pivot;
+	int pi;
 
 	if (low < high)
 	{
-		pivot = lomuto_partition(array, low, high);
-		quick_sort_recursive(array, low, pivot - 1);
-		quick_sort_recursive(array, pivot + 1, high);
+		pi = partition(array, low, high);
+
+		quick_sort_recursive(array, low, pi - 1);
+		quick_sort_recursive(array, pi + 1, high);
 	}
 }
 
@@ -83,3 +81,4 @@ void quick_sort(int *array, size_t size)
 
 	quick_sort_recursive(array, 0, size - 1);
 }
+
